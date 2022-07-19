d7ba9a29354fd237a36349c84e2743e407f408e9
JayJjee <joanguilherme.contato@gmail.com>
2022-07-19 00:50:08 -0300

    ex03

---
+++ ex03/index.js
@@ -0,0 +1,27 @@
+import {promises as fs} from "fs";
+
+writeReadJson();
+
+async function writeReadJson(){
+    try{
+
+        // escrito com valores iniciais
+        const arrayCarros = ["Gol", "Palio", "Uno"];
+        const obj = {
+            carros: arrayCarros
+        };
+        await fs.writeFile("teste.json", JSON.stringify(obj));
+
+        //leitura do conteudo atual
+        const data = JSON.parse(await fs.readFile("teste.json"));
+
+        //modificar o conteudo
+        data.carros.push("Sandero");
+        console.log(data)
+
+        //sobrescreve o arquivo com o countedo modificado
+        await fs.writeFile("teste.json", JSON.stringify(data));
+    } catch(err){
+        console.log(err);
+    }
+}
